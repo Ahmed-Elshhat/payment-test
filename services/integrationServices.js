80d9c1bb9fef95b4709f368dfec0b476ffdf41b2
Ahmed-Elshhat <169122740+Ahmed-Elshhat@users.noreply.github.com>
2025-02-09 23:05:19 +0200

    Update integrationServices.js

--- services/integrationServices.js
+++ services/integrationServices.js
@@ -1,3 +1,11 @@
+// Paymob API Keys
+const API_KEY =
+  "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2TVRBd05EZzNNeXdpYm1GdFpTSTZJakUzTXprd05EYzRNREV1TlRFeU1UQTRJbjAub3lJUGFDdWhocmR6N05uYk5lelVrU1BDT0N3Nnp3NFlXdnU3TkhfNmhPcG1UOTlPTDdoVHRLWG5zODBfYjFxVEdLblRFTC1vMG5oNWwza2pGZlhWYWc=";
+const SECRET_KEY =
+  "egy_sk_test_b602fcf2e9f8f0cd79fed785be4edcc260424fdf1f15c06d9ebd831144fe4883";
+const PUBLIC_KEY = "egy_pk_test_d3QY0JqYWxTIJ0sRVLlvoz3fhTBcESpM";
+
+
 exports.getAccessToken = async () => {
   try {
     const response = await axios.post(
